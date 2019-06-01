0a657f64a814a3e44dc409569e782bad87fd5daf
Your Name <damaris.golea@gmail.com>
2019-06-01 20:59:31 +0300

    added files for assignment2

---
+++ File1.java
@@ -0,0 +1,10 @@
+public class {
+	
+    public static void main (String[] args) {
+		System.out.println("Primul Fisier");
+	}
+	
+}
+
+// Acesta este un text pentru primul Fisier
+	
---
+++ File2.java
@@ -0,0 +1,9 @@
+public class {
+	
+    public static void main (String[] args) {
+		System.out.println("Al doilea Fisier");
+	}
+	
+}
+
+// Acesta este un text pentru cel de-al doilea Fisier
